047d0b72f162ed02565f679ca10cf286d223be4f
Acelerabit <regio.pimentel2016@gmail.com>
2023-03-12 19:52:07 -0300

    configura domain cliente

--- src/main/java/br/com/petz/clientepet/ClientePetApplication.java
+++ src/main/java/br/com/petz/clientepet/ClientePetApplication.java
@@ -1,11 +1,23 @@
 package br.com.petz.clientepet;
 
+
+import br.com.petz.clientepet.cliente.domain.Cliente;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.stream.XMLEventWriter;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePetApplication {
-
+	@GetMapping
+	public String getHomeTeste(){
+		return "Cliente Pet- API home";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePetApplication.class, args);
 	}

ed87d679156cf2f11b6a34b3faf3f96309fcd614
Acelerabit <regio.pimentel2016@gmail.com>
2023-03-13 17:14:41 -0300

    implementa contrato da API

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteAPI.java
@@ -0,0 +1,15 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/cliente") // é v1 pois é a primeira versão da api cliente.
+public interface ClienteAPI {
+    // aqui onde vamos mapear a parte do crud.
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    ClienteResponse postCliente(@RequestBody ClienteRequest clienteRequest);
+
+
+}

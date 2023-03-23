aab8bf2bc022e87042cec93c2a1ccea6b64aa1d9
Acelerabit <regio.pimentel2016@gmail.com>
2023-03-22 23:15:43 -0300

    implementa getTodosClientes

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClientelistResponse.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClientelistResponse.java
@@ -1,10 +1,13 @@
 package br.com.petz.clientepet.cliente.application.api;
 
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import lombok.Value;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class ClientelistResponse {
     private UUID idCliente;
     private String nomeCompleto;
@@ -13,6 +16,16 @@ public class ClientelistResponse {
     private String celular;
 
     public static List<ClientelistResponse> converte(List<Cliente> clientes) {
-        return null;
+        return clientes.stream()
+                .map(ClientelistResponse::new)
+                        .collect(Collectors.toList());
+    }
+    private ClientelistResponse (Cliente cliente){
+        super();
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.cpf = cliente.getCpf();
+        this.email = cliente.getEmail();
+        this.celular=cliente.getCelular();
     }
 }

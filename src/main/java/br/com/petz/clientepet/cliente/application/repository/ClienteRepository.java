dea7ebfce4c9614c651ee22d0395c7d42cdea52f
Acelerabit <regio.pimentel2016@gmail.com>
2023-03-25 14:53:30 -0300

    Configura handler

--- src/main/java/br/com/petz/clientepet/cliente/application/repository/ClienteRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/repository/ClienteRepository.java
@@ -3,10 +3,14 @@ package br.com.petz.clientepet.cliente.application.repository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ClienteRepository  {
 
         Cliente salva(Cliente cliente);
 
         List<Cliente> buscaTodosClientes();
+
+    Cliente buscaClienteAtravesId(UUID idCliente);
+
 }
--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteinfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteinfraRepository.java
@@ -2,11 +2,14 @@ package br.com.petz.clientepet.cliente.infra;
 
 import br.com.petz.clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.clientepet.cliente.domain.Cliente;
+import br.com.petz.clientepet.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 @Log4j2
@@ -27,5 +30,14 @@ public class ClienteinfraRepository implements ClienteRepository {
         List<Cliente> todosClientes=clienteSpringDataJPARepository.findAll();
         log.info("[Finaliza] ClienteinfraRepository - buscaTodosClientes  ");
         return todosClientes;
+    }
+
+    @Override
+    public Cliente buscaClienteAtravesId(UUID idCliente) {
+        log.info("[inicia] ClienteinfraRepository - buscaClienteAtravesId   ");
+        Cliente cliente = clienteSpringDataJPARepository.findByIdCliente(idCliente)
+                .orElseThrow(() -> APIException.build(HttpStatus.NOT_FOUND,"Cliente não encontrado!"));
+        log.info("[Finaliza] ClienteinfraRepository - buscaClienteAtravesId ");
+        return cliente;
     }
 }

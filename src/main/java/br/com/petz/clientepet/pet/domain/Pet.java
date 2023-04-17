599c37dcc1c30ef65f22f988f1755b1230feaa1f
Acelerabit <regio.pimentel2016@gmail.com>
2023-04-16 21:44:04 -0300

    implementa pet domain

---
+++ src/main/java/br/com/petz/clientepet/pet/domain/Pet.java
@@ -0,0 +1,43 @@
+package br.com.petz.clientepet.pet.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid",name = "idPet")
+    private UUID idPet;
+    @NotBlank
+    private String nomePet;
+    @Enumerated(EnumType.STRING)
+    private Porte porte;
+    @Enumerated(EnumType.STRING)
+    private TipoPet tipo;
+    private String microchip;
+    private String raca;
+    @Enumerated(EnumType.STRING)
+    private SexoPet sexo;
+    private String pelagemCor;
+    @NotNull
+    private LocalDate dataNascimento;
+    private String rga;
+    private Integer peso;
+    private LocalDateTime dataHoraDoCadastro;
+    private LocalDateTime dataHoraDaUltimaAlteracao;
+
+
+
+}
